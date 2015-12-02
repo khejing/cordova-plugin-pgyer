8c76af6b6c8c44aefd5ecfe1355a5c48f24dc431
khejing <khejing@hotmail.com>
2015-12-02 17:53:43 +0800

    update

--- src/android/Pgyer.java
+++ src/android/Pgyer.java
@@ -7,10 +7,43 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-/**
- * This class echoes a string called from JavaScript.
- */
+import com.pgyersdk.feedback.PgyFeedbackShakeManager;
+import com.pgyersdk.update.PgyUpdateManager;
+
 public class Pgyer extends CordovaPlugin {
+
+    @Override
+    public void initialize(final CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        cordova.getThreadPool().execute(new Runnable() {
+            public void run() {
+                PgyUpdateManager.register(cordova.getActivity());
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // 自定义摇一摇的灵敏度，默认为1000，数值越小灵敏度越高。
+        PgyFeedbackShakeManager.setShakingThreshold(1000);
+
+        // 以对话框的形式弹出
+        PgyFeedbackShakeManager.register(MainActivity.this);
+
+        // 以Activity的形式打开，这种情况下必须在AndroidManifest.xml配置FeedbackActivity
+        // 打开沉浸式,默认为false
+        // FeedbackActivity.setBarImmersive(true);
+        //PgyFeedbackShakeManager.register(MainActivity.this, false);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        PgyFeedbackShakeManager.unregister();
+    }
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
